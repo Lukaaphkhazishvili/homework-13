f066b0ae3ce007c79b2d5a59972260bb271f6e34
luka afxazishvili <lukaapkhazishvili@gmail.com>
2024-10-18 23:00:30 +0400

    js

---
+++ script.js
@@ -0,0 +1,20 @@
+// cvladebi
+let Name = "luka";
+let Lastname = "afxazishvili";
+let Age = 15;
+let Email = "afxazishvililuka@gmail.com";
+let Number = "597013372";
+let Adult = false;
+
+// asakis ganaxleba
+function New_Age(New_Age) {
+  Age = New_Age;
+}
+
+// console
+console.log("Name:", Name);
+console.log("Lastname:", Lastname);
+console.log("Age:", Age);
+console.log("Email:", Email);
+console.log("%c Adult: " + Adult, "color: yellow;");
+console.log("%c Number: " + Number, "color: red;");
